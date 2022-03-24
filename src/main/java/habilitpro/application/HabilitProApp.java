1c14e92384bcb6e05d804fc08634a8be74892896
Arthur Guerra <arthur_gb1996@hotmail.com>
2022-03-23 22:54:43 -0300

    transfere o main da classe Programa para classe HabilitProApp

--- src/main/java/habilitpro/application/HabilitProApp.java
+++ src/main/java/habilitpro/application/HabilitProApp.java
@@ -1,14 +1,54 @@
 package habilitpro.application;
 
+import habilitpro.connection.JpaConnectionFactory;
+import habilitpro.services.EmpresaService;
+import habilitpro.services.UsuarioService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Program {
+import javax.persistence.EntityManager;
+import java.util.Scanner;
 
-    private static final Logger LOG = LogManager.getLogger(Program.class);
+public class HabilitProApp {
+
+    private static final Logger LOG = LogManager.getLogger(HabilitProApp.class);
+
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        LOG.info("teste");
+        EntityManager entityManager = new JpaConnectionFactory().getEntityManager();
+        UsuarioService usuarioService = new UsuarioService(entityManager);
+        EmpresaService empresaService = new EmpresaService(entityManager);
+
+
+
+//        usuarioService.autenticaUsuarioESenha(getUsuario(), getSenha());
+
+//        Usuario usuario = new Usuario("Administrador", "89086607047", "adm@teste.com", "12345678a");
+//        Perfil perfil = new Perfil(usuario, PerfilAcessoEnum.administrativo);
+//        usuario.addPerfil(perfil);
+//        usuarioService.create(usuario);
+
+//        Empresa empresa = new Empresa(
+//                "SERVICO NACIONAL DE APRENDIZAGEM INDUSTRIAL",
+//                "05867982000137",
+//                true,
+//                "Instituto SENAI de Tecnologia em Calçado e Logística",
+//                SegmentoEnum.tic,
+//                "Novo Hamburgo",
+//                "Rio Grande do Sul",
+//                RegionalSenaiEnum.sul);
+
+    }
+
+    private static String getUsuario() {
+        System.out.print("Usuário: ");
+        return sc.nextLine();
+    }
+
+    private static String getSenha() {
+        System.out.print("Senha: ");
+        return sc.nextLine();
     }
 }
