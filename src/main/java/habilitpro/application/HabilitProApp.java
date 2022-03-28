3fd2812af8c27ae04cdcc9aa1e79531d9a51d5c4
Arthur Guerra <arthur_gb1996@hotmail.com>
2022-03-27 23:40:49 -0300

    último commit

--- src/main/java/habilitpro/application/HabilitProApp.java
+++ src/main/java/habilitpro/application/HabilitProApp.java
@@ -2,6 +2,7 @@ package habilitpro.application;
 
 import habilitpro.connection.JpaConnectionFactory;
 import habilitpro.enums.NotaEnum;
+import habilitpro.enums.PerfilAcessoEnum;
 import habilitpro.model.persistence.*;
 import habilitpro.services.*;
 import org.apache.logging.log4j.LogManager;
@@ -18,7 +19,6 @@ public class HabilitProApp {
     private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-
         EntityManager entityManager = new JpaConnectionFactory().getEntityManager();
         UsuarioService usuarioService = new UsuarioService(entityManager);
         EmpresaService empresaService = new EmpresaService(entityManager);
@@ -29,24 +29,10 @@ public class HabilitProApp {
         AvaliacaoService avaliacaoService = new AvaliacaoService(entityManager);
         SetorService setorService = new SetorService(entityManager);
         FuncaoService funcaoService = new FuncaoService(entityManager);
+        PerfilAcessoService perfilAcessoService = new PerfilAcessoService(entityManager);
+
 //        usuarioService.autenticaUsuarioESenha(getUsuario(), getSenha());
 
-
-
-//        Usuario usuario = new Usuario("Administrador", "89086607047", "adm@teste.com", "12345678a");
-//        Perfil perfil = new Perfil(usuario, PerfilAcessoEnum.administrativo);
-//        usuario.addPerfil(perfil);
-//        usuarioService.create(usuario);
-
-//        Empresa empresa = new Empresa(
-//                "SERVICO NACIONAL DE APRENDIZAGEM INDUSTRIAL",
-//                "05867982000137",
-//                true,
-//                "Instituto SENAI de Tecnologia em Calçado e Logística",
-//                SegmentoEnum.tic,
-//                "Novo Hamburgo",
-//                "Rio Grande do Sul",
-//                RegionalSenaiEnum.sul);
 
     }
 
